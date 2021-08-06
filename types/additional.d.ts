cff053c76ef35458d4c7bb9733da1ee87f6fe995
Toliak <toliakpurple@gmail.com>
2021-08-06 19:33:42 +0300

    Add prettier, issue templates, misc

--- types/additional.d.ts
+++ types/additional.d.ts
@@ -1,43 +1,4 @@
-export const root: any;
-
-export interface Vector3Type {
-    x: number;
-    y: number;
-    z: number;
-    add: LuaAdditionMethod<Vector3Type, Vector3Type>;
-    sub: LuaSubtractionMethod<Vector3Type, Vector3Type>;
-
-    getLength(): number;
-}
-
-export function Vector3(x: number, y: number, z: number): Vector3Type;
-
-export function setCameraMatrix(from: Vector3Type, to: Vector3Type, roll?: number, fov?: number): boolean
-
-export type EasingFunction =
-    | "Linear"
-    | "InQuad"
-    | "OutQuad"
-    | "InOutQuad"
-    | "OutInQuad"
-    | "InElastic"
-    | "OutElastic"
-    | "InOutElastic"
-    | "OutInElastic"
-    | "InBack"
-    | "OutBack"
-    | "InOutBack"
-    | "OutInBack"
-    | "InBounce"
-    | "OutBounce"
-    | "InOutBounce"
-    | "OutInBounce"
-    | "SineCurve"
-    | "CosineCurve";
-
-export function interpolateBetween(v1: Vector3Type, v2: Vector3Type, progress: number, easing: EasingFunction): LuaMultiReturn<[number, number, number]>;
-
-export class debug {
-    /** @noSelf */
-    static traceback(): any;
-}
+/** You can insert any not declared definitions here (or in another file in this folder)
+ * If you have any requests, you can easily submit them here:
+ * https://github.com/mtasa-typescript/mtasa-lua-types/issues
+ **/
